2d50b1a8ffd290c81e6fe0ef2200022e448116b0
lasini1022 <102490645+lasini1022@users.noreply.github.com>
2025-08-21 19:16:40 -0400

    added date and time with javascript

---
+++ index.js
@@ -0,0 +1,44 @@
+function showClocks() {
+  let torontoElement = document.querySelector("#toronto");
+  let torontoDateElement = torontoElement.querySelector(".date");
+  let torontoTimeElement = torontoElement.querySelector(".time");
+  torontoDateElement.innerHTML = moment()
+    .tz("America/Toronto")
+    .format("dddd, MMMM D, YYYY");
+  torontoTimeElement.innerHTML = moment()
+    .tz("America/Toronto")
+    .format("hh:mm:ss [<small>]A[</small>]");
+
+  let colomboElement = document.querySelector("#colombo");
+  let colomboDateElement = colomboElement.querySelector(".date");
+  let colomboTimeElement = colomboElement.querySelector(".time");
+  colomboDateElement.innerHTML = moment()
+    .tz("Asia/Colombo")
+    .format("dddd, MMMM D, YYYY");
+  colomboTimeElement.innerHTML = moment()
+    .tz("Asia/Colombo")
+    .format("hh:mm:ss [<small>]A[</small>]");
+
+  let adelaideElement = document.querySelector("#adelaide");
+  let adelaideDateElement = adelaideElement.querySelector(".date");
+  let adelaideTimeElement = adelaideElement.querySelector(".time");
+  adelaideDateElement.innerHTML = moment()
+    .tz("Australia/Adelaide")
+    .format("dddd, MMMM D, YYYY");
+  adelaideTimeElement.innerHTML = moment()
+    .tz("Australia/Adelaide")
+    .format("hh:mm:ss [<small>]A[</small>]");
+
+  let madridElement = document.querySelector("#madrid");
+  let madridDateElement = madridElement.querySelector(".date");
+  let madridTimeElement = madridElement.querySelector(".time");
+  madridDateElement.innerHTML = moment()
+    .tz("Europe/Madrid")
+    .format("dddd, MMMM D, YYYY");
+  madridTimeElement.innerHTML = moment()
+    .tz("Europe/Madrid")
+    .format("hh:mm:ss [<small>]A[</small>]");
+}
+
+showClocks();
+setInterval(showClocks, 1000);
